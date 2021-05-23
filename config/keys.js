103d483cb6c65bcff6830e4f6fc51838cfe662a6
jalloh-codes <saylujalloh@gmail.com>
2021-05-23 11:02:03 -0400

    delete post added

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = { 
-    // mongoURI: "mongodb://localhost:27017/gsa", 
-    mongoURI: 'mongodb+srv://cellouAdmin:WeCoding@1@cluster0.tjl3g.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    
-    secretOrKey: "secret" 
-} 
