8197e0e0b0ab44b521e9b3699da430154ec688a0
jalloh-codes <saylujalloh@gmail.com>
2021-09-11 14:53:39 -0400

    email validation added

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -310,6 +310,11 @@ const resolvers = {
             if(accountExist){
                 throw new Error("Email already existed")
             }
+            const validate = (email) => {
+                const expression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+            
+                return expression.test(String(email).toLowerCase())
+            }
             const hashPassword = await bcrypt.hash(args.input.password, 12);
             const user = new User({
                 email: args.input.email,
@@ -322,27 +327,31 @@ const resolvers = {
                 skills: args.input.skills,
                 interest: args.input.interest
             })
-            const result = await user.save()
-
-            const payload = await {
-                email: result.email,
-                id: result._id,
-                firstname: result.firstname,
-                lastname: result.lastname,
-                date: result.date
-            }
-
-            const token = await jwt.sign(
-                payload,
-                keys.secretOrKey,
-                {expiresIn: '365d'},
-            )
-
-            return{
-                token: 'Bearer ' + token,
-                success: true,
-                _id: result._id,
-                email: result.email,
+             
+            if(validate(user.email)){
+                const result = await user.save()
+            
+                const payload = await {
+                    email: result.email,
+                    id: result._id,
+                    firstname: result.firstname,
+                    lastname: result.lastname,
+                    date: result.date
+                }
+                const token = await jwt.sign(
+                    payload,
+                    keys.secretOrKey,
+                    {expiresIn: '365d'},
+                )
+                return{
+                    token: 'Bearer ' + token,
+                    success: true,
+                    _id: result._id,
+                    email: result.email,
+                }
+            }else{
+                console.log('Invalid');
+                throw new Error("Email not valid")
             }
         } catch (error) {
             console.log(error);
