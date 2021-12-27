96a77e79fc2b16cdf70a7bb80ce9ec6cb463c15b
jalloh-codes <saylujalloh@gmail.com>
2021-12-27 11:36:46 -0500

    make it better

--- config/keys.js
+++ config/keys.js
@@ -1,11 +1,12 @@
-export const aws = {
-    keyPrefix: "images/",
-    
+module.exports = { 
+    mongoURI: 'mongodb+srv://cellouAdmin:WeCoding@1@gsa.tjl3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: "secret",
+    email: 'blessmuss@gmail.com',
+    code: 'NakryOutLaw@1',
     bucketPost: 'gsa-image-store', 
     bucketProfile: 'gsa-profile-image',
     region: 'us-east-2',
     accessKey: 'AKIAVEQU6EBCSI4Z437G',
     secretKey: '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',
-}
 
-//arn:aws:iam::353304977477:policy/gsa-bucket-rules
+} 
