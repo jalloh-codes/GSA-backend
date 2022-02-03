a2e7daab4915b5bfab8541930de82f9bb22f01c3
jalloh-codes <saylujalloh@gmail.com>
2022-02-02 21:54:24 -0500

    remove files

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = { 
+    mongoURI: 'mongodb+srv://cellouAdmin:WeCoding@1@gsa.tjl3g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    secretOrKey: "secret",
+    email: 'blessmuss@gmail.com',
+    code: 'NakryOutLaw@1',
+    bucketPost: 'gsa-image-store', 
+    bucketProfile: 'gsa-profile-image',
+    region: 'us-east-2',
+    accessKey: 'AKIAVEQU6EBCSI4Z437G',
+    secretKey: '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',
+} 
+
+//config
+//git rm -r --cached .
